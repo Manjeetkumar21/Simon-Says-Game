f442a5572f5c714dbaad95f67ece82a674823927
Manjeet Kumar <21manjeetkumar21@gmail.com>
2024-02-22 22:45:40 +0530

    Adding Media Queries

--- script.js
+++ script.js
@@ -5,89 +5,96 @@ let btns = ["red", "yellow", "green", "blue"];
 
 let start = false;
 let level = 0;
-let h2 = document.querySelector('h2')
+let h2 = document.querySelector("h2");
 
-document.addEventListener('keypress',function(){
-    if(start == false){
-        console.log("Game is Started");
-        start = true;
+document.addEventListener("keypress", gameStart);
 
-        levelUp()
-    }
-})
+const startBtn = document.querySelector("#start");
 
-function levelUp(){
-    userSeq = []
-    level++;
-    h2.innerHTML = `Level ${level}`
-    h2.style.color = "black"
+startBtn.addEventListener("click", function () {
+  gameStart();
+  startBtn.style.display = "none";
+});
 
-    let randIdx = Math.floor(Math.random()*3)
-    let randColor = btns[randIdx]
-    let randBtn = document.querySelector(`.${randColor}`)
+function gameStart() {
+  if (start == false) {
+    console.log("Game is Started");
+    start = true;
 
-    gameSeq.push(randColor)
-    console.log(gameSeq);
-    gameFlash(randBtn)
-
+    levelUp();
+  }
 }
 
-function gameFlash(btn){
-    btn.classList.add('gameFlash')
-    setTimeout(() => {
-        btn.classList.remove('gameFlash')
-    }, 250);
+function levelUp() {
+  userSeq = [];
+  level++;
+  h2.innerHTML = `Level ${level}`;
+  h2.style.color = "black";
+
+  let randIdx = Math.floor(Math.random() * 3);
+  let randColor = btns[randIdx];
+  let randBtn = document.querySelector(`.${randColor}`);
+
+  gameSeq.push(randColor);
+  console.log(gameSeq);
+  gameFlash(randBtn);
 }
 
-function userFlash(btn){
-    btn.classList.add('userFlash')
-    setTimeout(() => {
-        btn.classList.remove('userFlash')
-    }, 250);
+function gameFlash(btn) {
+  btn.classList.add("gameFlash");
+  setTimeout(() => {
+    btn.classList.remove("gameFlash");
+  }, 250);
 }
 
-let allBtns = document.querySelectorAll(".button")
-
-function BtnClick(){
-    let btn = this;
-
-    userFlash(btn);
-    let userColor = btn.getAttribute('id');
-    userSeq.push(userColor);
-    console.log(userSeq);
-
-    checkAns(userSeq.length-1);
+function userFlash(btn) {
+  btn.classList.add("userFlash");
+  setTimeout(() => {
+    btn.classList.remove("userFlash");
+  }, 250);
 }
 
-allBtns.forEach(function(btns){
-    btns.addEventListener('click',BtnClick)
-})
+let allBtns = document.querySelectorAll(".button");
+
+function BtnClick() {
+  let btn = this;
+
+  userFlash(btn);
+  let userColor = btn.getAttribute("id");
+  userSeq.push(userColor);
+  console.log(userSeq);
+
+  checkAns(userSeq.length - 1);
+}
+
+allBtns.forEach(function (btns) {
+  btns.addEventListener("click", BtnClick);
+});
 
 // Check Answer
 
-function checkAns(idx){
-
-    if(userSeq[idx]===gameSeq[idx]){
-        if(userSeq.length == gameSeq.length){
-            setTimeout(levelUp,1000)
-        }
-    } else{
-        h2.style.color = "brown"
-        h2.innerHTML = `GAME OVER!<br><b style="color:blue;">Your Score was ${level}</b> <br> Press any key to start again`;
-        let body = document.querySelector('body');
-        body.style.backgroundColor =  'red'
-        setTimeout(function(){
-            body.style.backgroundColor = "white"
-        },150);
-        
-        reset();
+function checkAns(idx) {
+  if (userSeq[idx] === gameSeq[idx]) {
+    if (userSeq.length == gameSeq.length) {
+      setTimeout(levelUp, 1000);
     }
+  } else {
+    h2.style.color = "brown";
+    h2.innerHTML = `GAME OVER!<br><b style="color:blue;">Your Score was ${level}</b> <br> Press any key to start again`;
+    startBtn.style.display = "block";
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "red";
+    setTimeout(function () {
+      body.style.backgroundColor = "white";
+    }, 150);
+
+    reset();
+  }
 }
 
-function reset(){
-    start = false;
-    gameSeq = [];
-    userSeq = [];
-    level = 0;
-    
+function reset() {
+  start = false;
+  gameSeq = [];
+  userSeq = [];
+  level = 0;
 }
